e5d25dd8ca7a34ef926497ed7d4d51c75cb65251
Ayush Das <ayushdas@localhost.localdomain>
2021-05-19 00:58:16 +0530

    Almost Done

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.fields.related import ForeignKey
 from django.urls import reverse
+from tinymce.models import HTMLField
 
 User = get_user_model()
 
@@ -20,16 +22,40 @@ class Category(models.Model):
         return self.title
 
 
+class PostView(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = ForeignKey('Post', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    post = models.ForeignKey(
+        'Post', related_name='comments', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
 class Post(models.Model):
     title = models.CharField(max_length=500)
-    overview = models.TextField()
+    overview = models.CharField(max_length=1000)
     timestamp = models.DateTimeField(auto_now_add=True)
-    comment_count = models.IntegerField(default=0)
-    view_count = models.IntegerField(default=0)
+    content = HTMLField()
+    # comment_count = models.IntegerField(default=0)
+    # view_count = models.IntegerField(default=0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     thumbnail = models.ImageField()
     categories = models.ManyToManyField(Category)
     featured = models.BooleanField()
+    previous_post = models.ForeignKey(
+        'self', related_name='previous', on_delete=models.SET_NULL, blank=True, null=True)
+    next_post = models.ForeignKey(
+        'self', related_name='next', on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -38,3 +64,15 @@ class Post(models.Model):
         return reverse('post-detail', kwargs={
             'id': self.id,
         })
+
+    @property
+    def get_comments(self):
+        return self.comments.all().order_by('-timestamp')
+
+    @property
+    def comment_count(self):
+        return Comment.objects.filter(post=self).count()
+
+    @property
+    def view_count(self):
+        return PostView.objects.filter(post=self).count()
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,8 @@
 from django.db.models import Count, Q
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Post
+from .forms import CommentForm
+from .models import Post, PostView
 from marketing.models import SignUp
 
 
@@ -84,4 +85,26 @@ def blog(request):
 
 
 def post(request, id):
-    return render(request, 'post.html', {})
+    post = get_object_or_404(Post, id=id)
+    category_count = get_category_count()
+    most_recent = Post.objects.order_by('-timestamp')[:3]
+    form = CommentForm(request.POST or None)
+
+    PostView.objects.get_or_create(user=request.user, post=post)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.instance.user = request.user
+            form.instance.post = post
+            form.save()
+            return redirect(reverse('post-detail', kwargs={
+                'id': post.id,
+            }))
+
+    context = {
+        'form': form,
+        'post': post,
+        'most_recent': most_recent,
+        'category_count': category_count,
+    }
+    return render(request, 'post.html', context)
