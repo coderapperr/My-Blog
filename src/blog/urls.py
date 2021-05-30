40218f9172527faafd524ff47560aee8c8a3a033
Ayush Das <ayushdas@localhost.localdomain>
2021-05-30 15:28:07 +0530

    final

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 from posts.views import index, blog, post, search
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('MysEcretAdMiN/', admin.site.urls, name='admin'),
     path('', index),
     path('blog/', blog, name='post-list'),
     path('search/', search, name='search'),
